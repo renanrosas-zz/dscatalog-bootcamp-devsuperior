eb95a9f3c2e8e00dd2281ff964032e0c40a9daa7
Renan Rosas <renan-rosas@hotmail.com>
2021-06-03 12:08:26 -0300

    Custom ConstraintValidator

--- backend/src/main/java/com/renanrosas/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/renanrosas/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.renanrosas.dscatalog.dto;
 
+import com.renanrosas.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
 	private static final long serialVersionUID = 1L;
 	
