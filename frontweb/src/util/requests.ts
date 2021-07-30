95d3aafbd1f20b6b161934322a5c697349f77166
Renan Rosas <renan-rosas@hotmail.com>
2021-07-30 20:56:57 -0300

    Check user authenticated

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -1,6 +1,15 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import qs from 'qs';
 import history from './history';
+import jwtDecode from 'jwt-decode';
+
+type Role = 'ROLE_OPERATOR' | 'ROLE_ADMIN';
+
+type TokenData = {
+  exp: number;
+  username: string;
+  authorities: Role[];
+}
 
 type LoginResponse = {
   access_token: string;
@@ -62,6 +71,22 @@ export const getAuthData = () => {
   const str = localStorage.getItem(tokeyKey) ?? '{}';
   return JSON.parse(str) as LoginResponse;
 };
+
+export const getTokenData = () : TokenData | undefined => {
+
+  try {
+    return jwtDecode(getAuthData().access_token) as TokenData;
+  }
+  catch (error) {
+    return undefined;
+  }
+}
+
+export const isAuthenticated = () : boolean => {
+  const tokenData = getTokenData();
+
+  return (tokenData && tokenData.exp * 1000 > Date.now()) ? true : false;
+}
 
 // Add a request interceptor
 axios.interceptors.request.use(function (config) {
