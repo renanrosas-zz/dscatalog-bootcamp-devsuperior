220c10eb4de00125b0d25cdfb61dc4ebef5598c4
Renan Rosas <renan-rosas@hotmail.com>
2021-07-30 21:51:15 -0300

    Login/logout styling

--- frontweb/src/components/Navbar/index.tsx
+++ frontweb/src/components/Navbar/index.tsx
@@ -70,10 +70,10 @@ const Navbar = () => {
                     </ul>
                 </div>
 
-                <div>
+                <div className="nav-login-logout">
                     {authData.authenticated ? (
                         <>
-                        <span>{authData.tokenData?.user_name}</span>
+                        <span className="nav-username">{authData.tokenData?.user_name}</span>
                         <a href="#logout" onClick={handleClickLogoutClick}>LOGOUT</a>
                         </>
                     ) : (
