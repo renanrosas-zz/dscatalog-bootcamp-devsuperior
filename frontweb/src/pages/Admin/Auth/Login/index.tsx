7ab9a2ef924c9dbfcbb7b2fd142be9fd38b045d0
Renan Rosas <renan-rosas@hotmail.com>
2021-07-31 20:02:03 -0300

    Redirecting to intended route after login

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -1,19 +1,25 @@
-import { Link, useHistory } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import ButtonIcon from 'components/ButtonIcon';
 import { useForm } from "react-hook-form";
 import { getTokenData, requestBackendLogin, saveAuthData } from 'util/requests';
 import { useContext, useState } from 'react';
-
-import './styles.css';
 import { AuthContext } from 'AuthContext';
-
+import './styles.css';
 
 type FormData = {
   username: string,
   password: string
+};
+
+type LocationState = {
+  from: string
 }
 
 const Login = () => {
+
+  const location = useLocation<LocationState>();
+  
+  const { from } = location.state || { from: { pathname: '/admin' } };
 
   const { setAuthContextData } = useContext(AuthContext);
 
@@ -33,7 +39,7 @@ const Login = () => {
           authenticated: true,
           tokenData: getTokenData()
         });
-        history.push('/admin');
+        history.replace(from);
       })
       .catch(error => {
         setHasError(true);
