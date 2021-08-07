8e7b3c35db4c4d356fe880d32f49b2bd5d159310
Renan Rosas <renan-rosas@hotmail.com>
2021-08-06 21:22:05 -0300

    Form buttons styling

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -6,19 +6,27 @@ const Form = () => {
             <div className="base-card product-crud-form-card">
                 <h1>DADOS DO PRODUTO</h1>
                 <form action="">
-                    <div className="row">
-                        <div className="col-lg-6">
-                            <input type="text" className="form-control base-input" />
-                            <input type="text" className="form-control base-input" />
-                            <input type="text" className="form-control base-input" />
+                    <div className="row product-crud-inputs-container">
+                        <div className="col-lg-6 product-crud-inputs-left">
+                            <div className="margin-bottom-30 ">
+                                <input type="text" className="form-control base-input" />
+                            </div>
+                            <div className="margin-bottom-30 ">
+                                <input type="text" className="form-control base-input" />
+                            </div>
+                            <div>
+                                <input type="text" className="form-control base-input" />
+                            </div>
                         </div>
                         <div className="col-lg-6">
-                            <textarea name="" rows={10} className="form-control base-input"></textarea>
+                            <div>
+                                <textarea name="" rows={10} className="form-control base-input h-auto" />
+                            </div>
                         </div>
                     </div>
-                    <div>
-                        <button className="btn btn-outline-danger">CANCELAR</button>
-                        <button className="btn btn-primary">SALVAR</button>
+                    <div className="product-crud-form-buttons-container">
+                        <button className="btn btn-outline-danger product-form-btn">CANCELAR</button>
+                        <button className="btn btn-primary text-white product-form-btn">SALVAR</button>
                     </div>
                 </form>
             </div>
