f465d65f232bb56e9db29d39cb7e2b8d6b8ff93c
Renan Rosas <renan-rosas@hotmail.com>
2021-08-08 14:10:34 -0300

    Retrieving categories from back end

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -1,8 +1,9 @@
 import { AxiosRequestConfig } from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useHistory, useParams } from 'react-router-dom';
 import Select from 'react-select';
+import { Category } from 'types/category';
 import { Product } from 'types/product';
 import { requestBackend } from 'util/requests';
 import './styles.css';
@@ -13,19 +14,22 @@ type UrlParams = {
 
 const Form = () => {
 
-    const options = [
-        { value: 'chocolate', label: 'Chocolate' },
-        { value: 'strawberry', label: 'Strawberry' },
-        { value: 'vanilla', label: 'Vanilla' }
-      ]
-
     const { productId } = useParams<UrlParams>();
 
     const history = useHistory();
 
+    const [selectCategories, setSelectCategories] = useState<Category[]>([]);
+
     const { register, handleSubmit, formState: { errors }, setValue } = useForm<Product>();
 
     const isEditing = productId !== 'create';
+
+    useEffect(() => {
+        requestBackend({url: '/categories'})
+            .then(response => {
+                setSelectCategories(response.data.content);
+            })
+    }, []);
 
     useEffect(() => {
         if (isEditing) {
@@ -88,9 +92,11 @@ const Form = () => {
 
                             <div className="margin-bottom-30 ">
                                 <Select 
-                                    options={options}
+                                    options={selectCategories}
                                     classNamePrefix="product-crud-select"
                                     isMulti
+                                    getOptionLabel={(cateogry: Category) => cateogry.name}
+                                    getOptionValue={(cateogry: Category) => String(cateogry.id)}
                                 />
                             </div>
 
