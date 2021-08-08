3555fa330e8580416cbc3a512f44b73369fa34a8
Renan Rosas <renan-rosas@hotmail.com>
2021-08-08 14:03:30 -0300

    React select layout

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -2,6 +2,7 @@ import { AxiosRequestConfig } from 'axios';
 import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { useHistory, useParams } from 'react-router-dom';
+import Select from 'react-select';
 import { Product } from 'types/product';
 import { requestBackend } from 'util/requests';
 import './styles.css';
@@ -11,6 +12,12 @@ type UrlParams = {
 }
 
 const Form = () => {
+
+    const options = [
+        { value: 'chocolate', label: 'Chocolate' },
+        { value: 'strawberry', label: 'Strawberry' },
+        { value: 'vanilla', label: 'Vanilla' }
+      ]
 
     const { productId } = useParams<UrlParams>();
 
@@ -78,6 +85,15 @@ const Form = () => {
                                 />
                                 <div className="invalid-feedback d-block">{errors.name?.message}</div>
                             </div>
+
+                            <div className="margin-bottom-30 ">
+                                <Select 
+                                    options={options}
+                                    classNamePrefix="product-crud-select"
+                                    isMulti
+                                />
+                            </div>
+
                             <div className="margin-bottom-30 ">
                                 <input
                                     {...register("price", {
