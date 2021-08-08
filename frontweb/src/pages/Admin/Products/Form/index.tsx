76cdec4a7b3b2be64552ec6dc5ffa803d73cce00
Renan Rosas <renan-rosas@hotmail.com>
2021-08-08 14:45:01 -0300

    Float format

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -1,5 +1,6 @@
 import { AxiosRequestConfig } from 'axios';
 import { useEffect, useState } from 'react';
+import CurrencyInput from 'react-currency-input-field';
 import { useForm, Controller } from 'react-hook-form';
 import { useHistory, useParams } from 'react-router-dom';
 import Select from 'react-select';
@@ -47,10 +48,12 @@ const Form = () => {
 
     const onSubmit = (formData: Product) => {
 
+        const data = {...formData, price: String(formData.price).replace(',', '.')}
+
         const config: AxiosRequestConfig = {
             method: isEditing ? 'PUT' : 'POST',
             url: isEditing ? `/products/${productId}` : "/products",
-            data: formData,
+            data,
             withCredentials: true
         };
 
@@ -105,17 +108,23 @@ const Form = () => {
                             </div>
 
                             <div className="margin-bottom-30 ">
-                                <input
-                                    {...register("price", {
-                                        required: 'Campo obrigatório',
-                                    })}
-                                    type="text"
-                                    className={`form-control base-input ${errors.price ? 'is-invalid' : ''}`}
-                                    placeholder="Preço"
+                                <Controller
                                     name="price"
+                                    rules={{required: 'Campo obrigatório'}}
+                                    control={control}
+                                    render={({ field }) => (
+                                        <CurrencyInput
+                                            placeholder="Preço"
+                                            className={`form-control base-input ${errors.price ? 'is-invalid' : ''}`}
+                                            disableGroupSeparators={true}
+                                            value={field.value}
+                                            onValueChange={field.onChange}
+                                        />
+                                    )}
                                 />
                                 <div className="invalid-feedback d-block">{errors.price?.message}</div>
                             </div>
+
                             <div className="margin-bottom-30 ">
                                 <input
                                     {...register("imgUrl", {
