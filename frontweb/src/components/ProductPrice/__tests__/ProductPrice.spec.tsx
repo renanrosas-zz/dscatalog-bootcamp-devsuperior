5394a2df6cc3fe650146504efe22db408f4c04be
Renan Rosas <renan-rosas@hotmail.com>
2021-08-30 21:33:17 -0300

    ProductPrice, ProductPrice tests

---
+++ frontweb/src/components/ProductPrice/__tests__/ProductPrice.spec.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import ProductPrice from "..";
+
+
+
+test('ProductPrice should render ProductPrice', () => {
+    const price = 10.1;
+
+    render(
+        <ProductPrice price={price} />
+    );
+
+    expect(screen.getByText("R$")).toBeInTheDocument();
+    expect(screen.getByText("10,10")).toBeInTheDocument();
+})
