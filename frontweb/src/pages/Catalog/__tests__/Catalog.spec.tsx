ee5087ddf18c20da63db8dceaaa577dc41fbe7ea
Renan Rosas <renan-rosas@hotmail.com>
2021-09-01 21:55:22 -0300

    Catalog, waitFor, async-await

---
+++ frontweb/src/pages/Catalog/__tests__/Catalog.spec.tsx
@@ -0,0 +1,22 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import { Router } from "react-router-dom";
+import history from "util/history";
+import Catalog from "..";
+
+
+
+test('should render Catalog with products', async () => {
+
+    render(
+        <Router history={history}>
+            <Catalog />
+        </Router>
+    );
+
+    expect(screen.getByText('Catálogo de produtos')).toBeInTheDocument();
+
+    await waitFor(() => {
+        expect(screen.getByText('Smart TV')).toBeInTheDocument();
+    });
+
+});
