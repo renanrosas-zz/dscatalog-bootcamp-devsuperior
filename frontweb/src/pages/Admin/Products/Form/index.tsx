9b4e246ed70247db3b10fbf625d7e9eafd0da328
Renan Rosas <renan-rosas@hotmail.com>
2021-09-07 12:26:54 -0300

    Test should show toast and redirect when submit form correctly

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -88,11 +88,13 @@ const Form = () => {
                                     className={`form-control base-input ${errors.name ? 'is-invalid' : ''}`}
                                     placeholder="Nome do produto"
                                     name="name"
+                                    data-testid="name"
                                 />
                                 <div className="invalid-feedback d-block">{errors.name?.message}</div>
                             </div>
 
                             <div className="margin-bottom-30 ">
+                            <label htmlFor="categories" className="d-none">Categorias</label>
                                 <Controller
                                     name="categories"
                                     rules={{ required: true }}
@@ -104,6 +106,7 @@ const Form = () => {
                                             isMulti
                                             getOptionLabel={(category: Category) => category.name}
                                             getOptionValue={(category: Category) => String(category.id)}
+                                            inputId="categories"
                                         />
                                     )}
                                 />
@@ -124,6 +127,7 @@ const Form = () => {
                                             disableGroupSeparators={true}
                                             value={field.value}
                                             onValueChange={field.onChange}
+                                            data-testid="price"
                                         />
                                     )}
                                 />
@@ -143,6 +147,7 @@ const Form = () => {
                                     className={`form-control base-input ${errors.imgUrl ? 'is-invalid' : ''}`}
                                     placeholder="Url da imagem do produto"
                                     name="imgUrl"
+                                    data-testid="imgUrl"
                                 />
                                 <div className="invalid-feedback d-block">{errors.imgUrl?.message}</div>
                             </div>
@@ -157,6 +162,7 @@ const Form = () => {
                                     className={`form-control base-input h-auto ${errors.description ? 'is-invalid' : ''}`}
                                     placeholder="Descrição"
                                     name="description"
+                                    data-testid="description"
                                 />
                                 <div className="invalid-feedback d-block">{errors.description?.message}</div>
                             </div>
---
+++ frontweb/src/pages/Admin/Products/__tests__/Form.spec.tsx
@@ -0,0 +1,61 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Router, useParams } from "react-router-dom";
+import selectEvent from "react-select-event";
+import { ToastContainer } from "react-toastify";
+import history from "util/history";
+import Form from "../Form";
+import { server } from "./fixtures";
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useParams: jest.fn()
+}));
+
+describe('Product form create tests', () => {
+
+    beforeEach(() => {
+        (useParams as jest.Mock).mockReturnValue({
+            productId: 'create'
+        })
+    })
+
+    test('Should render Form', async () => {
+
+        render(
+            <Router history={history}>
+                <ToastContainer />
+                <Form />
+            </Router>
+
+        );
+
+        const nameInput = screen.getByTestId("name");
+        const priceInput = screen.getByTestId("price");
+        const imgUrlInput = screen.getByTestId("imgUrl");
+        const descriptionInput = screen.getByTestId("description");
+        const categoriesInput = screen.getByLabelText("Categorias");
+
+        const submitButton = screen.getByRole('button', { name: /salvar/i})
+
+        await selectEvent.select(categoriesInput, ['Eletrônicos', 'Computadores']);
+        userEvent.type(nameInput, 'Computador');
+        userEvent.type(priceInput, '5000.12');
+        userEvent.type(imgUrlInput, 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg');
+        userEvent.type(descriptionInput, 'Computador muito bom');
+
+        userEvent.click(submitButton);
+
+        await waitFor(() => {
+            const toastElement = screen.getByText('Produto cadastrado com sucesso');
+            expect(toastElement).toBeInTheDocument();
+        })
+
+        expect(history.location.pathname).toEqual('/admin/products');
+    
+    });
+});
